a84a7e4bfd997458c6e8457aa2443d1151c48dee
Joshua Desroches <josh.des.21@gmail.com>
2026-03-17 14:47:30 -0400

    this project is fucked

--- Assets/Scripts/PauseMenuController.cs
+++ Assets/Scripts/PauseMenuController.cs
@@ -4,19 +4,20 @@ using UnityEngine;
 public class PauseMenuController : MonoBehaviour
 {
     [SerializeField] String mainMenuSceneName = "MainMenu";
+    [SerializeField] GameObject pauseMenuUI;
 
     public void OnPaused()
     {
         // Pause the game
         Time.timeScale = 0f;
-        this.gameObject.SetActive(true);
+        pauseMenuUI.SetActive(true);
     }
 
     public void OnResumeButtonClicked()
     {
         // Resume the game
         Time.timeScale = 1f;
-        gameObject.SetActive(false);
+        pauseMenuUI.SetActive(false);
     }
 
     public void OnMainMenuButtonClicked()
@@ -24,5 +25,11 @@ public class PauseMenuController : MonoBehaviour
         // Load the main menu scene
         Time.timeScale = 1f;
         UnityEngine.SceneManagement.SceneManager.LoadScene(mainMenuSceneName);
+    }
+
+    public void OnQuitButtonClicked()
+    {
+        // Quit the application
+        Application.Quit();
     }
 }
--- Assets/Scripts/PlayerCollector.cs
+++ Assets/Scripts/PlayerCollector.cs
@@ -2,13 +2,22 @@ using UnityEngine;
 
 public class PlayerCollector : MonoBehaviour
 {
-    private SnowboardControlV2 snowboardController;
-    void OnCollisionEnter(Collision collision)
+    [SerializeField] private SnowboardControlV2 snowboardController;
+    [SerializeField] private GameObject GameOverScreen;
+
+    void OnTriggerEnter(Collider collision)
     {
         if (collision.gameObject.CompareTag("Collectible"))
         {
             // Collect the item
-            //snowboardController.score += 10;
+            snowboardController.AddScore(10f);
+            Destroy(collision.gameObject);
+        }
+        if (collision.gameObject.CompareTag("Checkpoint"))
+        {
+            // Handle endgame logic
+            snowboardController.AddScore(20f);
+            GameOverScreen.SetActive(true);
             Destroy(collision.gameObject);
         }
     }
